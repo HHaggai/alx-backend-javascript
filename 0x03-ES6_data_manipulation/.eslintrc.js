d227209c5e2c2573976c9513c6999406b5121d39
hhaggai <hillhaggai@outlook.com>
2024-03-12 12:08:56 -0700

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptons: {
+      ecmaVerson: 2018,
+      sourceTyp: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrids:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
+  };
